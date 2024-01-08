603f8648f94ebfd4d5f1e7d7aabcf0fcf72a0632
Amir Norozi <amirnrzdev002@gmail.com>
2024-01-08 13:38:27 +0330

    clean code refactor

--- main.js
+++ main.js
@@ -8,61 +8,51 @@
 
 
 // exports
-const Account = require('./classes/Account')
-const CommandLine = require('./classes/CommandLine')
+const Account = require('./classes/Account');
+const CommandLine = require('./classes/CommandLine');
 
-async function main() {
-
+(async () => {
   try {
-    const firstQuestion = 'wich account would you like to enter?'
-    const accountName = await CommandLine.ask(firstQuestion)
-    const account = await Account.find(accountName)
-  
-    if(account == null) {
-      await promptCreateAccount(accountName)
-    } else if(account) {
-      await propmtTask(account)
+    const accountName = await CommandLine.ask('Which account would you like to enter?');
+    let account = await Account.find(accountName);
+
+    if (!account) {
+      const shouldCreateAccount = await promptCreateAccount(accountName);
+      if (shouldCreateAccount) {
+        account = await Account.create(accountName);
+      }
     }
 
+    if (account) {
+      await promptTask(account);
+    }
   } catch (error) {
-    CommandLine.print("ERROR: please try again!")
+    CommandLine.print('ERROR: Please try again!');
   }
-
-
-}
-
+})();
 
 async function promptCreateAccount(accountName) {
-  const response = await CommandLine.ask("account you looking for does not exist would you like to create one? (yes/no)")
-
-  if(response === 'yes') {
-    return await Account.create(accountName)
-  }
+  const response = await CommandLine.ask('The account you are looking for does not exist. Would you like to create one? (yes/no)');
+  return response === 'yes';
 }
 
-async function propmtTask(account) {
-  const response = await CommandLine.ask("what would you like to do? (view/withdraw/deposit) ")
+async function promptTask(account) {
+  const response = await CommandLine.ask('What would you like to do? (view/withdraw/deposit)');
 
-  if(response === 'deposit') {
-    const amount =  parseFloat(await CommandLine.ask("how much?"))
-    await account.deposit(amount)
-    CommandLine.print(`you have added ${amount} to ${account.name} account and now your balance is ${account.balance}`)
-
-  } else if(response === 'withdraw') {
-    const amount =  parseFloat(await CommandLine.ask("how much?"))
+  if (response === 'deposit') {
+    const amount = parseFloat(await CommandLine.ask('How much?'));
+    await account.deposit(amount);
+    CommandLine.print(`You have added ${amount} to the ${account.name} account. Your balance is now ${account.balance}`);
+  } else if (response === 'withdraw') {
+    const amount = parseFloat(await CommandLine.ask('How much?'));
 
     try {
       await account.withdraw(amount);
-      CommandLine.print(`you have taken ${amount} from the ${account.name} account and now your balance is ${account.balance}`);
-
+      CommandLine.print(`You have withdrawn ${amount} from the ${account.name} account. Your balance is now ${account.balance}`);
     } catch (error) {
-      CommandLine.print("we were unabel to make the withdraw. please ensure you have enough money")
+      CommandLine.print('Unable to make the withdrawal. Please ensure you have enough money.');
     }
   } else {
-    CommandLine.print(`your balance is ${account.balance}`)
+    CommandLine.print(`Your balance is ${account.balance}`);
   }
-} 
-
-
-
-main()
+}
